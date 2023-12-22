f38c77260f0a1ce129e35ddebb515027a48fcb06
Mark Zealey <mark@markandruth.co.uk>
2023-12-22 09:10:03 +0000

    Add in redis_try_run function to do all error handling/logging

--- src/moya/service/redis.py
+++ src/moya/service/redis.py
@@ -1,13 +1,21 @@
+import logging
 import typing as t
 from contextlib import asynccontextmanager
 from functools import cache
 
 import redis.asyncio as aioredis
+from redis import Redis  # noqa: F401 for reexport
 from redis.backoff import ExponentialBackoff
-from redis.exceptions import ConnectionError, TimeoutError
+from redis.exceptions import (  # noqa: F401 for reexport
+    ConnectionError,
+    RedisError,
+    TimeoutError,
+)
 from redis.retry import Retry
 
 from moya.util.config import MoyaSettings
+
+logger = logging.getLogger("moya-redis")
 
 
 class RedisSettings(MoyaSettings):
@@ -64,9 +72,44 @@ async def redis(settings: RedisSettings = None) -> t.AsyncGenerator[aioredis.Red
             ... stuff with redis_conn ...
     except (TimeoutError, ConnectionError) as e:
         logger.exception("Redis connection error", exc_info=e)
+    except RedisError as e:
+        logger.exception("Other redis error", exc_info=e)
     """
     if settings is None:
         settings = redis_settings()
     conn = aioredis.Redis.from_pool(pool(settings))
+
+    # For some reason this occasionally happens during redis failover rather
+    # than from_pool just raising the exception
+    if not conn:
+        raise ConnectionError("Could not connect to redis")
     yield conn
     await conn.aclose()
+
+
+async def redis_try_run(coro: t.Callable[[t.Any], t.Awaitable[t.Any]]) -> t.Optional[t.Any]:
+    """
+    Run a coroutine and log and ignore redis-specific errors. Depending on
+    whether connection succeeds the coroutine may never even be run.
+
+    It will return the value returned by the coroutine, or None if there was a
+    redis-related error. Other exceptions are raised in the usual way.
+
+    Usage:
+
+    from moya.service.redis import redis_try_run, Redis
+
+    async def fetch(redis_conn: Redis) -> t.Optional[dict]:
+        return await redis_conn.get("key")
+
+    return await redis_try_run(fetch)
+    """
+    try:
+        async with redis() as redis_conn:
+            return await coro(redis_conn)
+    except (ConnectionError, TimeoutError) as e:
+        logger.exception("Redis connection error", exc_info=e)
+        return None
+    except RedisError as e:
+        logger.exception("Redis general error", exc_info=e)
+        return None
