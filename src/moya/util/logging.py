930fef645e2bf4b39d7e7c94657b5f44a201fa06
Mark Zealey <mark@markandruth.co.uk>
2023-12-29 14:46:25 +0000

    Add standardized logging

---
+++ src/moya/util/logging.py
@@ -0,0 +1,22 @@
+import logging
+from typing import Literal
+
+from moya.util.config import MoyaSettings
+
+# Basic setup/config for python logging
+
+
+class LoggingSettings(MoyaSettings):
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "WARNING"
+
+
+log_settings = LoggingSettings()
+
+def setup_logging():
+    """
+    Initial logging setup
+    """
+    logging.basicConfig(
+        level=logging.getLevelName(log_settings.log_level),
+        format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+    )
