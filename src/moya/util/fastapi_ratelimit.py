edd5528b54c159d95d6fc56a7dff012587fd3099
Mark Zealey <mark@markandruth.co.uk>
2024-01-10 18:47:19 +0000

    Add FastAPI ratelimiting library
    Unfortunately no ratelimiting libraries seem to handle:
    - Async redis
    - Ability to use redis sentinel
    - Simple integration with FastAPI
    - Expiring of redis keys over time
    - Configuration from environment variables
    - Flushing an individual user's limits (eg for when account is reset)
    
    This library should do all of this and more

---
+++ src/moya/util/fastapi_ratelimit.py
@@ -0,0 +1,163 @@
+import typing as t
+
+from fastapi import Depends, HTTPException, status
+
+from moya.util.config import MoyaSettings
+
+from .ratelimit import LimitBase, RateLimit, RateLimitExceeded, RedisLimiter
+
+"""
+Easy library for using FastAPI with Ratelimiters
+"""
+
+
+class RateLimitSettings(MoyaSettings):
+    """
+    Extract environment variable APP_RATELIMITS and use it to configure
+    ratelimiters
+    """
+
+    ratelimits: dict[str, RateLimit]
+
+    def get(self, endpoint: str, default: RateLimit = None) -> RateLimit:
+        """
+        Get the ratelimiter for the given endpoint name, if exists
+        """
+        if endpoint in self.ratelimits:
+            return self.ratelimits[endpoint]
+        elif "*" in self.ratelimits:
+            return self.ratelimits["*"]
+        elif default is not None:
+            return default
+        else:
+            raise KeyError(
+                f"No ratelimits configured for limiter '{endpoint}'."
+                " Ensure that APP_RATELIMITS contains at least an entry for '*',"
+                " or that default_limits is set in the config"
+            )
+
+
+async def _null_fn() -> None:
+    """
+    Bypass all rate-limits when no rate limits have been configured for this item
+    """
+    pass
+
+
+class _RateLimiter:
+    def __init__(
+        self,
+        limiter: LimitBase,
+        user_id_decorator: t.TypeAlias = t.Annotated[None, Depends(_null_fn)],
+    ) -> None:
+        """
+        Create a new rate limiter with the given rates. This class is intended
+        to wrap details about different limiters and allow for easy integration
+        into FastAPI.
+
+        :param limiter: The limiter to use. This should be an instance of LimitBase.
+        :param user_id_decorator: The decorator to use to get the user_id from
+            the request. This should be an t.Annotated function (probably using
+            Depends()) returning a string or None. If the function returns None
+            then no ratelimits will be applied to this user (which is
+            potentially useful for admin users to bypass ratelimits).
+
+        """
+        self.limiter = limiter
+        self.user_id_decorator = user_id_decorator
+
+    async def reset_user(self, user_id: str) -> None:
+        """
+        Clear all ratelimits for this limiter for the specified user
+        """
+        await self.limiter.flush_user(user_id)
+
+    async def reset_all(self) -> None:
+        """
+        Reset all ratelimits for all users using this limiter
+        """
+        await self.limiter.reset()
+
+    @property
+    def dependency(self) -> t.Any:  # Any is the type of Depends() anyway
+        if self.limiter.rates.is_empty:
+            return Depends(_null_fn)
+
+        async def fn(user_id: self.user_id_decorator) -> None:  # type: ignore
+            # TODO: Could use dynamic rate limits here on a per-path basis by
+            # using something like the following as the key prefix:
+            # [request.method, request['route'].path]
+
+            if user_id is None:
+                return
+
+            try:
+                await self.limiter.try_ratelimit(user_id)
+            except RateLimitExceeded:
+                raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="Too many requests")
+
+        return Depends(fn)
+
+
+def RateLimiter(
+    name: str,
+    user_id_decorator: t.Any,  # Must be a Depends() function which returns a str or None
+    default_limits: t.Optional[RateLimit] = None,
+    limiter_class: t.Type[LimitBase] = RedisLimiter,
+) -> _RateLimiter:
+    """
+    Easy fastapi dependency to create a rate limiter from environment vars.
+
+    :param name: The name of the limiter to use. This should be the same as the
+        name of the endpoint you want to limit and will have limiter config
+        pulled from that item in the APP_RATELIMITS config variable.
+    :param user_id_decorator: The decorator to use to get the user_id from the
+        request. This must be specified and should be a Depends() function
+        returning a str or None for no ratelimit for this particular user.
+    :param default_limits: The default limits to use if no limits are specified
+        in the environment, otherwise if no default limits are specified in the
+        environment a startup-time error will be raised.
+    :param limiter_class: The class to use for the limiter. This should be a
+        subclass of LimitBase. This defaults to RedisLimiter, but could be set
+        to MemLimiter for testing.
+
+    Usage:
+
+    async def ensure_verified(
+        credentials: t.Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]
+    ):
+        return credentials.username
+
+    limiter = RateLimiter("foo", Depends(ensure_verified))
+    @app.get("/foo", dependencies=[limiter.dependency])
+    async def foo():
+        await limiter.flush_user("foo@bar.com")
+
+    and set APP_RATELIMITS='{"foo": {"per_minute": 10, "per_day": 20}}' in your environment
+    """
+
+    settings = RateLimitSettings()
+    return _RateLimiter(
+        limiter_class(settings.get(name, default_limits), name), user_id_decorator=t.Annotated[str, user_id_decorator]
+    )
+
+
+def RateLimiterDep(
+    name: str,
+    user_id_decorator: t.TypeAlias,
+    default_limits: t.Optional[RateLimit] = None,
+    limiter_class: t.Type[LimitBase] = RedisLimiter,
+) -> t.Any:
+    """
+    As RateLimeter above but returns the dependency directly. This is useful to
+    save typing when you don't need to flush or reset the ratelimiter.
+
+    Usage with setup above:
+
+    @app.get("/foo", dependencies=[RateLimiterDep("foo", Depends(ensure_verified))])
+    async def foo():
+        pass
+    """
+    return RateLimiter(
+        name, user_id_decorator=user_id_decorator, default_limits=default_limits, limiter_class=limiter_class
+    ).dependency
---
+++ src/moya/util/ratelimit.py
@@ -0,0 +1,181 @@
+import abc
+import time
+from functools import cached_property
+
+from pydantic import BaseModel
+
+from moya.service.redis import Redis, redis_try_run
+from moya.util.background import run_in_background
+
+"""
+Generic ratelimit-related stuff, should have no FastAPI dependencies
+"""
+
+
+class RateLimit(BaseModel):
+    per_second: int = None
+    per_minute: int = None
+    per_hour: int = None
+    per_day: int = None
+
+    class Config:
+        keep_untouched = (cached_property,)
+
+    @cached_property
+    def rates(self) -> list[tuple[int, int]]:
+        return [
+            (limit, duration)
+            for limit, duration in (
+                (self.per_second, 1),
+                (self.per_minute, 60),
+                (self.per_hour, 60 * 60),
+                (self.per_day, 24 * 60 * 60),
+            )
+            if limit is not None
+        ]
+
+    @property
+    def max_duration(self) -> int:
+        return self.rates[-1][1]
+
+    @property
+    def is_empty(self) -> bool:
+        return self.per_second is None and self.per_minute is None and self.per_hour is None and self.per_day is None
+
+
+class RateLimitExceeded(Exception):
+    """
+    Raised because the given endpoint's ratelimit has been exceeded
+    """
+
+    pass
+
+
+class LimitBase:
+    """
+    Base class for rate limiters
+    """
+
+    def __init__(self, rates: RateLimit, base_key: str) -> None:
+        self.rates = rates
+        self.base_key = base_key
+        if len(self.key("")) < 4:
+            raise ValueError("base_key is too short. Must be at least 4 characters")
+
+    def key(self, user_id: str) -> str:
+        """
+        Given a user_id return the base key to use for the ratelimiter
+        """
+        return ":".join([self.base_key, user_id])
+
+    @abc.abstractmethod
+    async def try_ratelimit(self, user_id: str) -> None:
+        """
+        raise RateLimitExceeded exception if ratelimit is exceeded, otherwise return normally.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def flush_user(self, user_id: str) -> None:
+        """
+        Flush the given user's ratelimit for this limiter
+        """
+        pass
+
+    @abc.abstractmethod
+    async def reset(self) -> None:
+        """
+        Reset everything to do with this limiter
+        """
+        pass
+
+
+class MemLimiter(LimitBase):
+    """
+    A memory-based limiter using a dictionary to store the timestamps of each
+    request. This is mostly used for testing and should not be used in
+    production.
+    """
+
+    def __init__(self, rates: RateLimit, base_key: str) -> None:
+        super().__init__(rates, base_key)
+        self._reset()
+
+    async def try_ratelimit(self, user_id: str) -> None:
+        now = time.time()
+        cur_limits = self._limits.setdefault(self.key(user_id), [])
+
+        for limit, duration in self.rates.rates:
+            if sum(1 for t in cur_limits if t > now - duration) >= limit:
+                raise RateLimitExceeded()
+
+        cur_limits.append(now)
+
+    async def flush_user(self, user_id: str) -> None:
+        del self._limits[self.key(user_id)]
+
+    def _reset(self) -> None:
+        # Ordered list of timestamps for the given limit. Could set it to purge
+        # after a certain timestamp if we wanted, but this limiter is mostly
+        # just used for testing anyway.
+        self._limits: dict[str, list[float]] = {}
+
+    async def reset(self) -> None:
+        self._reset()
+
+
+class RedisLimiter(LimitBase):
+    """
+    A Redis-based limiter using sorted sets to store the timestamps each user
+    accessed a given function. If there is any issue with redis connectivity
+    this should fail open, perhaps with a couple of seconds delay to the calls.
+    """
+
+    async def try_ratelimit(self, user_id: str) -> None:
+        now = time.time()
+        key = self.key(user_id)
+
+        async def check_rates(redis: Redis) -> None:
+            # Use pipelining to ensure only 1 round-trip
+            async with redis.pipeline() as pipe:
+                for limit, duration in self.rates.rates:
+                    pipe.zcount(key, now - duration, now)
+                results = await pipe.execute()
+
+            for (limit, duration), count in zip(self.rates.rates, results):
+                if count >= limit:
+                    raise RateLimitExceeded()
+
+        await redis_try_run(check_rates, readonly=True)
+
+        async def cleanup_rates(redis: Redis) -> None:
+            async with redis.pipeline() as pipe:
+                # Log the request
+                pipe.zadd(key, {str(now): now})  # TODO: Better key name - random id perhaps?
+
+                # Set expiry for the key as a whole (you cannot set for the individual items)
+                pipe.expire(key, self.rates.max_duration)
+
+                # Remove old items from the list
+                pipe.zremrangebyscore(key, 0, now - self.rates.max_duration)
+                await pipe.execute()
+
+        await run_in_background(redis_try_run(cleanup_rates))
+
+    async def flush_user(self, user_id: str) -> None:
+        async def cleanup_rates(redis: Redis) -> None:
+            await redis.delete(self.key(user_id))
+
+        await redis_try_run(cleanup_rates)
+
+    async def reset(self) -> None:
+        async def reset_redis(redis: Redis) -> None:
+            cursor = 0
+            while True:
+                cursor, keys = await redis.scan(cursor, match=self.key("*"), count=1000)
+                if keys:
+                    await redis.delete(*keys)
+                if cursor == 0:
+                    break
+
+        await redis_try_run(reset_redis)
