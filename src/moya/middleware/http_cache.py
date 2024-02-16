1f5acfd81d68cacb9c42a08220708b1d0b9e7890
Mark Zealey <mark@markandruth.co.uk>
2024-02-16 12:52:31 +0000

    Add in HTTP Cache middleware functionality

---
+++ src/moya/middleware/http_cache.py
@@ -0,0 +1,86 @@
+import typing as t
+from email.utils import parsedate
+
+from fastapi import HTTPException, Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.staticfiles import NotModifiedResponse
+
+
+def should_return_304(request: Request, response: Response) -> bool:
+    if_modified_since = parsedate(request.headers.get("If-Modified-Since"))
+    last_modified = parsedate(response.headers.get("last-modified"))
+
+    if (
+        request.method in ("GET", "HEAD")
+        and if_modified_since
+        and last_modified
+        and if_modified_since >= last_modified
+    ):
+        return True
+
+    return False
+
+
+class IfModifiedSinceMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: t.Callable[[Request], t.Awaitable[Response]]) -> Response:
+        """
+        If the response has a last-modified header which is older than the
+        if-modified-since header, return a 304 without content to optimize
+        bandwidth and rely on the client's cache.
+
+        This middleware is only needed if the response has a last-modified
+        header and skip_if_not_modified was not set below.
+        """
+        response = await call_next(request)
+
+        if should_return_304(request, response):
+            return NotModifiedResponse(response.headers)
+
+        return response
+
+
+def set_cache_headers(
+    request: Request,
+    response: Response,
+    last_modified: str = None,
+    max_age: int = None,
+    stale_if_error: int = 60 * 60,
+    public: bool = True,
+    skip_if_not_modified: bool = True,
+) -> None:
+    """
+    Set cache headers on the response
+
+    :param response: The response object to set headers on
+    :param last_modified: The last modified date of the content (as a http header string)
+    :param max_age: The maximum time the content should be cached for as an integer
+    :param stale_if_error: The time the content can be used if there is a server error. Defaults to 1 hour
+    :param public: If the content can be cached by public caches. Defaults to True
+    :param skip_if_not_modified: If the further processing should be skipped
+        via an HTTPException response if the If-Modified-Since header is present
+        and the content has not been modified. Defaults to True
+    """
+    # Handle the cache-control header
+    cache_control = []
+    if max_age:
+        cache_control.append(f"max-age={max_age}")
+    if stale_if_error:
+        cache_control.append(f"stale-if-error={stale_if_error}")
+    if public:
+        cache_control.append("public")
+    response.headers["Cache-Control"] = ", ".join(cache_control)
+
+    if last_modified:
+        response.headers["last-modified"] = last_modified
+
+        # See if we should just return a 304 no updated data response based on the last-mod time
+        if skip_if_not_modified and should_return_304(request, response):
+            # Same as a NotModifiedResponse but bypassing further processing
+            raise HTTPException(
+                status_code=304,
+                headers={
+                    name: value
+                    for name, value in response.headers.items()
+                    if name in NotModifiedResponse.NOT_MODIFIED_HEADERS
+                },
+            )
---
+++ tests/middleware/test_http_cache.py
@@ -0,0 +1,94 @@
+import typing as t
+
+import httpx
+from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
+
+from moya.middleware.http_cache import IfModifiedSinceMiddleware, set_cache_headers
+
+
+async def test_cache_headers(subtests: t.Any) -> None:
+    app = FastAPI()
+    client = httpx.AsyncClient(app=app, base_url="http://test")
+
+    class TestItem(BaseModel):
+        name: str
+
+    @app.get("/item")
+    async def read_item(request: Request, response: Response) -> TestItem:
+        set_cache_headers(request, response, last_modified="Fri, 16 Feb 2024 10:37:38 GMT", max_age=3600)
+        return TestItem(name="Foo")
+
+    for headers in (
+        t.cast(dict[str, str], {}),
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:37 GMT"},
+        {"If-Modified-Since": "Invalid data"},
+    ):
+        with subtests.test(f"Headers: {headers}"):
+            response = await client.get("/item", headers=headers)
+            assert response.headers["cache-control"] == "max-age=3600, stale-if-error=3600, public"
+            assert response.headers["last-modified"] == "Fri, 16 Feb 2024 10:37:38 GMT"
+            assert response.status_code == 200
+            assert response.json() == {"name": "Foo"}
+
+    for headers in (
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:38 GMT"},
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:39 GMT"},
+    ):
+        with subtests.test(f"Headers: {headers}"):
+            response = await client.get("/item", headers=headers)
+            assert response.headers["cache-control"] == "max-age=3600, stale-if-error=3600, public"
+            assert "last-modified" not in response.headers
+            assert response.status_code == 304
+            assert response.text == ""
+
+    @app.post("/item")
+    async def create_item(test_item: TestItem, response: Response, request: Request) -> TestItem:
+        set_cache_headers(request, response, last_modified="Fri, 16 Feb 2024 10:37:38 GMT", max_age=3600)
+        return test_item
+
+    for headers in (
+        t.cast(dict[str, str], {}),
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:37 GMT"},
+        {"If-Modified-Since": "Invalid data"},
+    ):
+        with subtests.test(f"Headers: {headers}"):
+            response = await client.post("/item", json={"name": "Foo"}, headers=headers)
+            assert response.headers["cache-control"] == "max-age=3600, stale-if-error=3600, public"
+            assert response.headers["last-modified"] == "Fri, 16 Feb 2024 10:37:38 GMT"
+            assert response.status_code == 200
+            assert response.json() == {"name": "Foo"}
+
+
+async def test_if_modified_middleware() -> None:
+    app = FastAPI()
+    client = httpx.AsyncClient(app=app, base_url="http://test")
+    app.add_middleware(IfModifiedSinceMiddleware)
+
+    class TestItem(BaseModel):
+        name: str
+
+    @app.get("/item")
+    async def read_item(request: Request, response: Response) -> TestItem:
+        set_cache_headers(request, response, last_modified="Fri, 16 Feb 2024 10:37:38 GMT", max_age=3600)
+        return TestItem(name="Foo")
+
+    for headers in (
+        t.cast(dict[str, str], {}),
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:37 GMT"},
+    ):
+        response = await client.get("/item", headers=headers)
+        assert response.headers["cache-control"] == "max-age=3600, stale-if-error=3600, public"
+        assert response.headers["last-modified"] == "Fri, 16 Feb 2024 10:37:38 GMT"
+        assert response.status_code == 200
+        assert response.json() == {"name": "Foo"}
+
+    # But when the If-Modified-Since header is later than the last-modified
+    # header, it should return 304 and no content
+    for headers in (
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:38 GMT"},
+        {"If-Modified-Since": "Fri, 16 Feb 2024 10:37:39 GMT"},
+    ):
+        response = await client.get("/item", headers=headers)
+        assert response.status_code == 304
+        assert response.text == ""
