2d0e786b835b5aa404861c5c9b7fd38a41c02279
Mark Zealey <mark@markandruth.co.uk>
2024-02-27 07:20:01 +0000

    Add in asyncpool

---
+++ src/moya/util/asyncpool.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+import typing as t
+from contextlib import asynccontextmanager
+
+logger = logging.getLogger("asyncpool")
+
+T = t.TypeVar("T")
+
+
+async def _run_worker(queue: asyncio.Queue[T], fn: t.Callable[[T], t.Awaitable[None]]) -> None:
+    while True:
+        item = await queue.get()
+
+        # Wrap in exception handler to still catch errors
+        try:
+            await fn(item)
+        except Exception as e:
+            logger.exception(f"Error processing item {item}", exc_info=e)
+        finally:
+            queue.task_done()
+
+
+@asynccontextmanager
+async def asyncpool(
+    fn: t.Callable[[T], t.Awaitable[None]], worker_count: int = 5, maxsize: int = 0
+) -> t.AsyncIterator[t.Callable[[T], t.Awaitable[None]]]:
+    """
+    Run a pool of workers to process items from a queue. The queue is returned
+    from the context manager and can be used to enqueue items for processing.
+
+    The queue is automatically closed when the context manager exits.
+
+    Exceptions in the worker function will be logged but never passed back to the caller.
+
+    :param fn: The function to run for each item in the queue.
+    :param worker_count: The number of workers to run
+    :param maxsize: The maximum size of the queue. If 0, the queue is unbounded.
+
+    Usage:
+
+    async def worker(item):
+        ... do stuff ...
+
+    async def ...:
+        async with asyncpool(worker, worker_count=10) as enqueue:
+            for i in range(100):
+                await enqueue(i)
+    """
+
+    queue: asyncio.Queue = asyncio.Queue(maxsize=maxsize)
+    workers: t.List[asyncio.Task] = []
+
+    for i in range(worker_count):
+        workers.append(asyncio.create_task(_run_worker(queue, fn)))
+
+    try:
+        yield queue.put
+    finally:
+        await queue.join()
+        for worker in workers:
+            worker.cancel()
+        await asyncio.gather(*workers, return_exceptions=True)
