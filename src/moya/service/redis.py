moya-app/moya-pythonlib-util

commit e475fecd43cbcc4948d62fa4875c6d8a7e014503

Stricter mypy tests

src/moya/service/redis.py
@@ -68,7 +68,7 @@ def _standard_connection_kwargs(settings: RedisSettings, decode_responses: bool 
     Return a dict of standard connection kwargs for all redis pools
     """
     # Retry only applies to trying to get a connection, not if an individual connection times out.
-    retry = Retry(ExponentialBackoff(), settings.redis_connect_retries)
+    retry = Retry(ExponentialBackoff(), settings.redis_connect_retries)  # type: ignore
     return {
         "encoding": "utf-8",
         "decode_responses": decode_responses,
@@ -83,7 +83,7 @@ def _standard_connection_kwargs(settings: RedisSettings, decode_responses: bool 
 @cache
 def sentinel(settings: RedisSettings, **kwargs: t.Any) -> aioredis.sentinel.Sentinel:
     "Create a redis sentinel client"
-    return aioredis.sentinel.Sentinel(
+    return aioredis.sentinel.Sentinel(  # type: ignore
         settings.redis_sentinel_hosts,
         **_standard_connection_kwargs(settings, **kwargs),
         sentinel_kwargs={
@@ -96,12 +96,14 @@ def sentinel(settings: RedisSettings, **kwargs: t.Any) -> aioredis.sentinel.Sent
 @cache
 def pool(settings: RedisSettings, **kwargs: t.Any) -> aioredis.ConnectionPool:
     "Create a redis connection pool"
+    if not settings.redis_url:
+        raise ValueError("Redis URL is not set")
     return aioredis.ConnectionPool.from_url(settings.redis_url, **_standard_connection_kwargs(settings, **kwargs))
 
 
 @asynccontextmanager
 async def redis(
-    readonly: bool = False, settings: RedisSettings = None, **kwargs: t.Any
+    readonly: bool = False, settings: RedisSettings | None = None, **kwargs: t.Any
 ) -> t.AsyncGenerator[aioredis.Redis, None]:
     """
     Return a redis connection from the pool, and close it correctly when
@@ -143,7 +145,7 @@ async def redis(
     await conn.aclose()
 
 
-Result = t.TypeVar("Result")
+Result = t.Any
 
 
 async def redis_try_run(
@@ -180,7 +182,7 @@ CachedFunc = t.Callable[..., t.Awaitable[Result]]
 
 
 class RedisCached:
-    def __init__(self, func: CachedFunc, key: str, expiry: int = None, cache_none: bool = True) -> None:
+    def __init__(self, func: CachedFunc, key: str, expiry: int | None = None, cache_none: bool = True) -> None:
         self.func = func
         self.key = key
         self.expiry = expiry
@@ -198,7 +200,7 @@ class RedisCached:
 
         cached = await redis_try_run(fetch, readonly=True, decode_responses=False)
         if cached:
-            return t.cast(Result, cached)
+            return cached
 
         result = t.cast(Result, await self.func(*args, **kwargs))
         if result is None and not self.cache_none:
@@ -223,7 +225,9 @@ class RedisCached:
         await redis_try_run(delete)
 
 
-def redis_cached(key: str, expiry: int = None, cache_none: bool = True) -> t.Callable[[CachedFunc], RedisCached]:
+def redis_cached(
+    key: str, expiry: int | None = None, cache_none: bool = True
+) -> t.Callable[[CachedFunc], RedisCached]:
     """
     Decorator to cache the result of a function in redis
 

pyproject.toml
@@ -34,13 +34,13 @@ plugins = [
 ]
 python_version = 3.11
 ignore_missing_imports = true
-disallow_any_generics = false  # Use List[int] instead of List or list
+disallow_any_generics = true   # Require list[int] instead of list
 namespace_packages = true      # For OpenTelemetry to work
 disallow_untyped_defs = true   # All functions should have type annotations
 warn_redundant_casts = true    # Casting a variable to its own type is disallowed
 strict_equality = true         # Error if both sides of a comparison are unequal
-strict_optional = false        # Treat `None` as a legal value for every type.
-disallow_untyped_calls = false # Don't allow calling untyped functions from typed ones
+strict_optional = true         # Require explicitly specifying None
+disallow_untyped_calls = true  # Don't allow calling untyped functions from typed ones
 warn_return_any = true         # Don't allow returning Any from a typed function
 disallow_any_unimported = true # Show warning when missing type hints cause an imported object to be Any
 warn_unreachable = true        # Warn on unreachable code
@@ -48,7 +48,9 @@ enable_error_code = "redundant-expr" # Warn on redundant expressions
 
 [[tool.mypy.overrides]]
 module = ["tests.*"]
+# Allow some laziness in the tests
 disallow_untyped_defs = false
+disallow_any_generics = false
 
 [tool.poe.tasks]
 test = "pytest . --cov=src --cov-report=term"

src/moya/middleware/gzip_route.py
@@ -1,7 +1,7 @@
 # TODO: We should rename this to inbound_compression or similar because it
 # handles more than just gzip now.
 import zlib
-from typing import Callable
+from typing import Any, Callable, Coroutine
 
 import brotli
 from fastapi import Response
@@ -42,7 +42,7 @@ class GzipRoute(APIRoute):
     on.
     """
 
-    def get_route_handler(self) -> Callable:
+    def get_route_handler(self) -> Callable[[Request], Coroutine[Any, Any, Response]]:
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:

src/moya/middleware/http_cache.py
@@ -43,8 +43,8 @@ class IfModifiedSinceMiddleware(BaseHTTPMiddleware):
 def set_cache_headers(
     request: Request,
     response: Response,
-    last_modified: str | int | float | datetime = None,
-    max_age: int = None,
+    last_modified: str | int | float | datetime | None = None,
+    max_age: int | None = None,
     stale_if_error: int = 60 * 60,
     public: bool = True,
     skip_if_not_modified: bool = True,

src/moya/service/kafka.py
@@ -21,7 +21,7 @@ class KafkaSettings(MoyaSettings):
     kafka_password: str
     kafka_brokers: str
 
-    def as_kafka(self) -> dict:
+    def as_kafka(self) -> dict[str, t.Any]:
         "Return settings as a dict suitable for passing to aiokafka"
 
         return {
@@ -39,7 +39,7 @@ class KafkaBase:
     def __init__(self, settings: KafkaSettings, startup_timeout: int = 20) -> None:
         self.startup_timeout = startup_timeout
         self.settings = settings
-        self.started: asyncio.Future[bool] = None
+        self.started: asyncio.Future[bool] | None = None
         self.kafka: t.Any = None  # should be AIOKafkaProducer or AIOKafkaConsumer
 
     async def start(self) -> None:

src/moya/service/kafka_producer.py
@@ -45,8 +45,12 @@ class KafkaProducer(KafkaBase):
         await super().start()
 
     async def send_nowait(
-        self, topic: str, payload: dict, timestamp_ms: int = None, encoder: t.Type[json.JSONEncoder] | None = None
-    ) -> asyncio.Future:  # [aiokafka.RecordMetadata]:
+        self,
+        topic: str,
+        payload: dict[str, t.Any],
+        timestamp_ms: int | None = None,
+        encoder: t.Type[json.JSONEncoder] | None = None,
+    ) -> asyncio.Future[t.Any]:  # [aiokafka.RecordMetadata]:
         """
         Send a message to kafka and return a future which will be resolved when
         the message send has been completed
@@ -63,10 +67,14 @@ class KafkaProducer(KafkaBase):
 
         fut = await self.kafka.send(topic, json.dumps(payload, cls=encoder).encode("utf-8"), timestamp_ms=timestamp_ms)
         # return t.cast(asyncio.Future[aiokafka.RecordMetadata], fut)
-        return t.cast(asyncio.Future, fut)
+        return t.cast(asyncio.Future[t.Any], fut)
 
     async def send(
-        self, topic: str, payload: dict, timestamp_ms: int = None, encoder: t.Type[json.JSONEncoder] | None = None
+        self,
+        topic: str,
+        payload: dict[str, t.Any],
+        timestamp_ms: int | None = None,
+        encoder: t.Type[json.JSONEncoder] | None = None,
     ) -> t.Any:  # aiokafka.RecordMetadata:
         """
         Send a message to kafka and wait for it to successfully complete. This
@@ -79,7 +87,7 @@ class KafkaProducer(KafkaBase):
 
 
 @cache
-def kafka_producer(settings: KafkaSettings = None) -> KafkaProducer:
+def kafka_producer(settings: KafkaSettings | None = None) -> KafkaProducer:
     if settings is None:
         settings = KafkaSettings()
     return KafkaProducer(settings)

src/moya/util/asyncpool.py
@@ -48,11 +48,8 @@ async def asyncpool(
                 await enqueue(i)
     """
 
-    queue: asyncio.Queue = asyncio.Queue(maxsize=maxsize)
-    workers: t.List[asyncio.Task] = []
-
-    for i in range(worker_count):
-        workers.append(asyncio.create_task(_run_worker(queue, fn)))
+    queue: asyncio.Queue[T] = asyncio.Queue(maxsize=maxsize)
+    workers = [asyncio.create_task(_run_worker(queue, fn)) for _ in range(worker_count)]
 
     try:
         yield queue.put

src/moya/util/background.py
@@ -1,11 +1,11 @@
 import asyncio
 import logging
-from typing import Awaitable
+from typing import Any, Awaitable
 
 bg_logger = logging.getLogger("background-tasks")
 
 
-async def background_task_wrapper(task: Awaitable) -> None:
+async def background_task_wrapper(task: Awaitable[Any]) -> None:
     """
     Run the given task logging exceptions to sentry
     """
@@ -15,7 +15,7 @@ async def background_task_wrapper(task: Awaitable) -> None:
         bg_logger.exception("Exception in background task", exc_info=e)
 
 
-async def run_in_background(task: Awaitable) -> None:
+async def run_in_background(task: Awaitable[Any]) -> None:
     """
     Run a task in the background returning immediately, logging exceptions if
     they occur.

commit 6a25dfd1361b45761456e448bcd92e181c406bfa

Try to fix redis typing regressed by e475fec

src/moya/service/redis.py
@@ -145,7 +145,7 @@ async def redis(
     await conn.aclose()
 
 
-Result = t.Any
+Result = t.TypeVar("Result")
 
 
 async def redis_try_run(
@@ -178,11 +178,10 @@ async def redis_try_run(
         return None
 
 
-CachedFunc = t.Callable[..., t.Awaitable[Result]]
-
-
 class RedisCached:
-    def __init__(self, func: CachedFunc, key: str, expiry: int | None = None, cache_none: bool = True) -> None:
+    def __init__(
+        self, func: t.Callable[..., t.Awaitable[Result]], key: str, expiry: int | None = None, cache_none: bool = True
+    ) -> None:
         self.func = func
         self.key = key
         self.expiry = expiry
@@ -192,19 +191,23 @@ class RedisCached:
         return f"pickle:{self.key}:{args}:{kwargs}"
 
     async def __call__(self, *args: t.Any, **kwargs: t.Any) -> Result:
-        async def fetch(redis_conn: Redis) -> t.Any:
+        async def fetch(redis_conn: Redis) -> Result | None:
             data = await redis_conn.get(self.get_cache_key(args, kwargs))
             if data:
-                return pickle.loads(data)
+                return t.cast(Result, pickle.loads(data))
             return None
 
         cached = await redis_try_run(fetch, readonly=True, decode_responses=False)
         if cached:
             return cached
 
-        result = t.cast(Result, await self.func(*args, **kwargs))
-        if result is None and not self.cache_none:
-            return result
+        result = await self.func(*args, **kwargs)
+
+        # TypeVar of Result may include a return of None, or it may not - we
+        # don't know. MyPy thinks this statement in unreachable because it
+        # doesn't believe the return from here could be None though.
+        if result is None and not self.cache_none:  # type: ignore
+            return result  # type: ignore
 
         # Dump the result to a local variable as it would be saved to redis
         # here before the background task runs. There may be a race if the
@@ -227,7 +230,7 @@ class RedisCached:
 
 def redis_cached(
     key: str, expiry: int | None = None, cache_none: bool = True
-) -> t.Callable[[CachedFunc], RedisCached]:
+) -> t.Callable[[t.Callable[..., t.Awaitable[Result]]], RedisCached]:
     """
     Decorator to cache the result of a function in redis
 
@@ -241,3 +244,25 @@ def redis_cached(
         return RedisCached(func, key, expiry, cache_none=cache_none)
 
     return decorator
+
+
+def _mypy_test_fn() -> None:
+    """
+    Validation that @redis_cached wraps return type correctly. Cannot put in
+    test suite because that doesnt have the same level of checking as actual
+    code. Code not intended to ever be executed
+    """
+
+    @redis_cached(key="internal_test")
+    async def _test_cached() -> int:
+        return 1
+
+    async def foo() -> int:
+        return await _test_cached()
+
+    @redis_cached(key="internal_test2")
+    async def _test_cached2() -> int | None:
+        return None
+
+    async def foo2() -> int | None:
+        return await _test_cached2()