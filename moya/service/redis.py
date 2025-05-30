d3aa1af8ec424e0fa36f47c1fe78ece04e2c474c
Mark Zealey <mark@markandruth.co.uk>
2025-05-30 19:01:23 +0100

    Refactor redis client to a class maintaining back-compatibility

--- moya/service/redis.py
+++ moya/service/redis.py
@@ -1,8 +1,8 @@
 import logging
 import pickle
 import typing as t
-from contextlib import asynccontextmanager
-from functools import cache
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
+from functools import lru_cache
 
 import redis.asyncio as aioredis
 from pydantic import model_validator
@@ -97,150 +97,156 @@ class RedisSettings(MoyaSettings):
         return bool(self.redis_sentinel_hosts)
 
 
-@cache
-def redis_settings() -> RedisSettings:
-    """
-    Lazy load Redis settings from env vars
-    """
-    return RedisSettings()
-
-
-def _standard_connection_kwargs(settings: RedisSettings, decode_responses: bool = True) -> dict[str, t.Any]:
-    """
-    Return a dict of standard connection kwargs for all redis pools
-    """
-    # Retry only applies to trying to get a connection, not if an individual connection times out.
-    retry = Retry(ExponentialBackoff(), settings.redis_connect_retries)  # type: ignore
-    return {
-        "encoding": "utf-8",
-        "decode_responses": decode_responses,
-        "password": settings.redis_password,
-        "socket_connect_timeout": settings.redis_timeout,
-        "socket_timeout": settings.redis_timeout,
-        "retry": retry,
-        "retry_on_error": [ConnectionError, TimeoutError],
-    }
-
-
-def _pool_kwargs(settings: RedisSettings) -> dict[str, t.Any]:
-    return {
-        "max_connections": settings.redis_max_connections,
-        "timeout": settings.redis_pool_timeout,
-    }
-
-
-def _sentinel_connection_kwargs(settings: RedisSettings, **kwargs: t.Any) -> dict[str, t.Any]:
-    return {
-        **_standard_connection_kwargs(settings, **kwargs),
-        "password": settings.redis_sentinel_password or settings.redis_password,
-    }
-
-
-@cache
-def sentinel(settings: RedisSettings, **kwargs: t.Any) -> aioredis.sentinel.Sentinel:
-    "Create a redis sentinel client"
-    return aioredis.sentinel.Sentinel(  # type: ignore
-        settings.redis_sentinel_hosts,
-        **_standard_connection_kwargs(settings, **kwargs),
-        sentinel_kwargs=_sentinel_connection_kwargs(settings, **kwargs),
-    )
-
-
-@cache
-def sentinel_pool(service_name: str, readonly: bool, settings: RedisSettings, **kwargs: t.Any) -> aioredis.ConnectionPool:
-    # Like .master_for()/.slave_for() but returning the pool instead
-    return BlockingSentinelConnectionPool(  # type: ignore
-        service_name,
-        sentinel(settings, **kwargs),
-        is_master=not readonly,
-        check_connection=True,
-        **_pool_kwargs(settings),
-        connection_class=MoyaSentinelManagedSSLConnection if kwargs.get("ssl", False) else MoyaSentinelManagedConnection,
-        **_sentinel_connection_kwargs(settings, **kwargs),
-    )
-
-
-@cache
-def pool(settings: RedisSettings, **kwargs: t.Any) -> aioredis.ConnectionPool:
-    "Create a redis connection pool"
-    if not settings.redis_url:
-        raise ValueError("Redis URL is not set")
-    return aioredis.BlockingConnectionPool.from_url(settings.redis_url, **_standard_connection_kwargs(settings, **kwargs), **_pool_kwargs(settings))
-
-
-@asynccontextmanager
-async def redis(readonly: bool = False, settings: RedisSettings | None = None, **kwargs: t.Any) -> t.AsyncGenerator[aioredis.Redis, None]:
-    """
-    Return a redis connection from the pool, and close it correctly when
-    completed. If sentinel is available in the config it will prefer to use
-    that.
-
-    :param readonly: If specified it may choose to use a slave server to connect to
-
-    Usage:
-
-    from moya.service.redis import redis, ConnectionError, TimeoutError
-
-    try:
-        async with redis() as redis_conn:
-            content = await redis_conn.get("key")
-            ... stuff with redis_conn ...
-    except (TimeoutError, ConnectionError) as e:
-        logger.exception("Redis connection error", exc_info=e)
-    except RedisError as e:
-        logger.exception("Other redis error", exc_info=e)
-    """
-    if settings is None:
-        settings = redis_settings()
-
-    if settings.is_sentinel:
-        p = sentinel_pool(settings.redis_sentinel_service, readonly, settings, **kwargs)
-    else:
-        p = pool(settings, **kwargs)
-
-    # Use the pool, but don't auto-close it when aclose() is called, just give back the connection for future
-    # reuse.
-    client = aioredis.Redis(connection_pool=p)
-
-    # For some reason this occasionally happens during redis failover rather
-    # than from_pool just raising the exception
-    if not client:
-        raise ConnectionError("Could not connect to redis")
-    try:
-        yield client
-    finally:
-        await client.aclose()  # Releases the connection to the pool but doesn't close it
-
-
 Result = t.TypeVar("Result")
 
 
-async def redis_try_run(coro: t.Callable[[aioredis.Redis], t.Awaitable[Result]], readonly: bool = False, **kwargs: t.Any) -> t.Optional[Result]:
+class MoyaRedisClient:
     """
-    Run a coroutine and log and ignore redis-specific errors. Depending on
-    whether connection succeeds the coroutine may never even be run.
-
-    It will return the value returned by the coroutine, or None if there was a
-    redis-related error. Other exceptions are raised in the usual way.
-
-    Usage:
-
-    from moya.service.redis import redis_try_run, Redis
-
-    async def fetch(redis_conn: Redis) -> t.Optional[dict]:
-        return await redis_conn.get("key")
-
-    return await redis_try_run(fetch)
+    Intended to be used as a singleton context manager/decorator for a given configuration
     """
-    try:
-        async with redis(readonly, **kwargs) as redis_conn:
-            return await coro(redis_conn)
-    except (ConnectionError, TimeoutError) as e:
-        logger.exception("Redis connection error", exc_info=e)
-        return None
-    except RedisError as e:
-        logger.exception("Redis general error", exc_info=e)
-        return None
+
+    def __init__(self, settings: RedisSettings | None = None) -> None:
+        # Lazy init of settings
+        self._settings = settings
+
+    @property
+    def settings(self) -> RedisSettings:
+        if self._settings is None:
+            self._settings = RedisSettings()
+        return self._settings
+
+    def _standard_connection_kwargs(self, decode_responses: bool = True) -> dict[str, t.Any]:
+        """
+        Return a dict of standard connection kwargs for all redis pools
+        """
+        # Retry only applies to trying to get a connection, not if an individual connection times out.
+        retry = Retry(ExponentialBackoff(), self.settings.redis_connect_retries)  # type: ignore
+        return {
+            "encoding": "utf-8",
+            "decode_responses": decode_responses,
+            "password": self.settings.redis_password,
+            "socket_connect_timeout": self.settings.redis_timeout,
+            "socket_timeout": self.settings.redis_timeout,
+            "retry": retry,
+            "retry_on_error": [ConnectionError, TimeoutError],
+        }
+
+    def _pool_kwargs(self) -> dict[str, t.Any]:
+        return {
+            "max_connections": self.settings.redis_max_connections,
+            "timeout": self.settings.redis_pool_timeout,
+        }
+
+    def _sentinel_connection_kwargs(self, **kwargs: t.Any) -> dict[str, t.Any]:
+        return {
+            **self._standard_connection_kwargs(**kwargs),
+            "password": self.settings.redis_sentinel_password or self.settings.redis_password,
+        }
+
+    @lru_cache(maxsize=16)
+    def sentinel(self, **kwargs: t.Any) -> aioredis.sentinel.Sentinel:
+        "Create a redis sentinel client"
+        return aioredis.sentinel.Sentinel(  # type: ignore
+            self.settings.redis_sentinel_hosts,
+            **self._standard_connection_kwargs(**kwargs),
+            sentinel_kwargs=self._sentinel_connection_kwargs(**kwargs),
+        )
+
+    @lru_cache(maxsize=16)
+    def sentinel_pool(self, service_name: str, readonly: bool, **kwargs: t.Any) -> aioredis.ConnectionPool:
+        # Like .master_for()/.slave_for() but returning the pool instead
+        return BlockingSentinelConnectionPool(  # type: ignore
+            service_name,
+            self.sentinel(**kwargs),
+            is_master=not readonly,
+            check_connection=True,
+            **self._pool_kwargs(),
+            connection_class=MoyaSentinelManagedSSLConnection if kwargs.get("ssl", False) else MoyaSentinelManagedConnection,
+            **self._sentinel_connection_kwargs(**kwargs),
+        )
+
+    @lru_cache(maxsize=16)
+    def pool(self, **kwargs: t.Any) -> aioredis.ConnectionPool:
+        "Create a redis connection pool"
+        if not self.settings.redis_url:
+            raise ValueError("Redis URL is not set")
+        return aioredis.BlockingConnectionPool.from_url(self.settings.redis_url, **self._standard_connection_kwargs(**kwargs), **self._pool_kwargs())
+
+    def get_connection(self, readonly: bool = False, **kwargs: t.Any) -> AbstractAsyncContextManager[aioredis.Redis]:
+        """
+        Return a redis connection from the pool, and close it correctly when
+        completed. If sentinel is available in the config it will prefer to use
+        that.
+
+        :param readonly: If specified it may choose to use a slave server to connect to
+
+        Usage:
+
+        from moya.service.redis import redis, ConnectionError, TimeoutError
+
+        try:
+            async with redis() as redis_conn:
+                content = await redis_conn.get("key")
+                ... stuff with redis_conn ...
+        except (TimeoutError, ConnectionError) as e:
+            logger.exception("Redis connection error", exc_info=e)
+        except RedisError as e:
+            logger.exception("Other redis error", exc_info=e)
+        """
+        if self.settings.is_sentinel:
+            p = self.sentinel_pool(self.settings.redis_sentinel_service, readonly, **kwargs)
+        else:
+            p = self.pool(**kwargs)
+
+        @asynccontextmanager
+        async def _client() -> t.AsyncGenerator[aioredis.Redis, None]:
+            # Use the pool, but don't auto-close it when aclose() is called, just give back the connection for future
+            # reuse.
+            client = aioredis.Redis(connection_pool=p)
+
+            # For some reason this occasionally happens during redis failover rather
+            # than from_pool just raising the exception
+            if not client:
+                raise ConnectionError("Could not connect to redis")
+            try:
+                yield client
+            finally:
+                await client.aclose()  # Releases the connection to the pool but doesn't close it
+
+        return _client()
+
+    async def try_run(self, coro: t.Callable[[aioredis.Redis], t.Awaitable[Result]], readonly: bool = False, **kwargs: t.Any) -> t.Optional[Result]:
+        """
+        Run a coroutine and log and ignore redis-specific errors. Depending on
+        whether connection succeeds the coroutine may never even be run.
+
+        It will return the value returned by the coroutine, or None if there was a
+        redis-related error. Other exceptions are raised in the usual way.
+
+        Usage:
+
+        from moya.service.redis import redis_try_run, Redis
+
+        async def fetch(redis_conn: Redis) -> t.Optional[dict]:
+            return await redis_conn.get("key")
+
+        return await redis_try_run(fetch)
+        """
+        try:
+            async with self.get_connection(readonly, **kwargs) as redis_conn:
+                return await coro(redis_conn)
+        except (ConnectionError, TimeoutError) as e:
+            logger.exception("Redis connection error", exc_info=e)
+            return None
+        except RedisError as e:
+            logger.exception("Redis general error", exc_info=e)
+            return None
+
+
+# Export historical names for backwards compatibility
+_redis = MoyaRedisClient()
+redis = _redis.get_connection
+redis_try_run = _redis.try_run
 
 
 class RedisCached(t.Generic[Result]):
