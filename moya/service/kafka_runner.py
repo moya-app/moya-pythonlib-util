befa5172f96c5b1e7d3297ee7a6d82ecbf7b0d3a
Mark Zealey <mark@markandruth.co.uk>
2025-06-24 17:50:05 +0100

    Extend kafka runner to handle multiple records in parallel to fully exhaust CPU when being used

--- moya/service/kafka_runner.py
+++ moya/service/kafka_runner.py
@@ -8,7 +8,8 @@ from opentelemetry import propagate, trace
 from opentelemetry.instrumentation.aiokafka.utils import _aiokafka_getter
 from opentelemetry.trace import Status, StatusCode
 
-from moya.service.kafka_consumer import KafkaConsumer, KafkaSettings
+from moya.service.kafka_consumer import ConsumerRecord, KafkaConsumer, KafkaSettings
+from moya.util.asyncpool import asyncpool_queue
 
 # OTEL other instrumentation is set up automatically by opentelemetry-instrument -a as we won't be forking
 tracer = trace.get_tracer(__name__)
@@ -36,15 +37,34 @@ class KafkaRunner(abc.ABC, Generic[T]):
     async def main() -> None:
         setup_logging()
         setup_sentry()
-        await KafkaUserDetailUpdater().run()
+        await KafkaUserDetailUpdater(worker_count=5).run()
 
     if __name__ == "__main__":
         asyncio.run(main())
+
+    If you are inserting records into a database, because of the network latency you likely want to run with
+    `worker_count=5` or so to ensure that the process_item() tasks run in parallel so that full CPU can be used.
     """
 
-    def __init__(self, consumer_group: str, topics: list[str]) -> None:
+    def __init__(
+        self,
+        consumer_group: str,
+        topics: list[str],
+        kafka_batch_size: int = 100,
+        worker_count: int = 1,
+    ) -> None:
+        """
+        :param consumer_group: The consumer group name to use
+        :param topics: The list of topics to consume from
+        :param kafka_batch_size: The number of records to fetch from Kafka at a time. The commit only happens after all
+            of these records have been processed so any bugs or process exit will cause this whole batch to be retried
+            by the next process meaning you may get duplicates but you should never loose records.
+        :param worker_count: The number of parallel instances of process_item() to run at a time
+        """
         self.consumer_group = consumer_group
         self.topics = topics
+        self.kafka_batch_size = kafka_batch_size
+        self.worker_count = worker_count
         self.consumer = KafkaConsumer(
             KafkaSettings(),
             consumer_group,
@@ -56,6 +76,7 @@ class KafkaRunner(abc.ABC, Generic[T]):
             enable_auto_commit=True,
             auto_offset_reset="latest",
         )
+        self.counter = 0
 
     @abc.abstractmethod
     def parse_record(self, topic: str, value: bytes) -> tuple[T, str] | None:
@@ -70,54 +91,68 @@ class KafkaRunner(abc.ABC, Generic[T]):
         "Process the item - insert into a database or similar"
         ...
 
+    async def _process_record(self, record: ConsumerRecord) -> None:
+        """
+        Process the given record from Kafka.
+        """
+        # Pull the propagated trace from the consumer message so it ties back to the original request
+        # TODO: Move this into a pythonlib util helper function.
+        context = propagate.extract(record.headers, getter=_aiokafka_getter)
+        with tracer.start_as_current_span("process-record", context=context) as span:
+            try:
+                result = self.parse_record(record.topic, record.value)
+                if result is None:
+                    return
+                item, logging_key = result
+            except Exception as e:
+                logger.exception(f"Received invalid JSON input for {record.value} (topic: {record.topic})", exc_info=e)
+                span.set_status(Status(StatusCode.ERROR))
+                span.set_attribute("error.message", str(e))
+                span.set_attribute("error.input", record.value)
+                # await run_in_background(self.consumer.kafka.commit())  # commit after logging the error
+                return
+
+            timestamp = datetime.fromtimestamp(record.timestamp / 1000)
+
+            try:
+                await self.process_item(item, timestamp)
+                self.counter += 1
+            except Exception as e:
+                span.set_status(Status(StatusCode.ERROR))
+                span.set_attribute("error.message", str(e))
+                span.set_attribute("error.input", record.value)
+
+                logger.exception(f"Error processing {logging_key} {record.value}", exc_info=e)
+                # Don't commit because we want to retry probably???
+            else:
+                # Doesn't matter if this fails for some reason, we can always re-process the record but we
+                # don't want to loose any records
+                # await run_in_background(self.consumer.kafka.commit())
+
+                logger.debug(f"Processed for {logging_key} lag {datetime.now() - timestamp}")
+                if self.counter % 1000 == 0:
+                    # TODO: Alert sentry if lag > 30 min or something? But only once... Look at avg
+                    # lag of the last few records.
+                    logger.info(f"Processed {self.counter} items, last lag for partition {record.partition} was {datetime.now() - timestamp}")
+
     async def run(self) -> None:
-        # TODO: Batch into a single txn for a number of updates in order to reduce database load?
-        async with self.consumer.run():
-            counter = 0
+        async with (
+            self.consumer.run(),
+            asyncpool_queue(self._process_record, worker_count=self.worker_count, maxsize=self.worker_count * 3) as queue,
+        ):
             while True:
                 try:
-                    record = await self.consumer.getone()
+                    records = await self.consumer.getmany(max_records=self.kafka_batch_size)
                 except Exception as e:
                     logger.exception("Unknown Kafka exception", exc_info=e)
                     await asyncio.sleep(0.1)
                     continue
 
-                # Pull the propagated trace from the consumer message so it ties back to the original request
-                # TODO: Move this into a pythonlib util helper function.
-                context = propagate.extract(record.headers, getter=_aiokafka_getter)
-                with tracer.start_as_current_span("process-record", context=context) as span:
-                    try:
-                        result = self.parse_record(record.topic, record.value)
-                        if result is None:
-                            continue
-                        item, logging_key = result
-                    except Exception as e:
-                        logger.exception(f"Received invalid JSON input for {record.value} (topic: {record.topic})", exc_info=e)
-                        span.set_status(Status(StatusCode.ERROR))
-                        span.set_attribute("error.message", str(e))
-                        span.set_attribute("error.input", record.value)
-                        # await run_in_background(self.consumer.kafka.commit())  # commit after logging the error
-                        continue
+                for topic, record_list in records.items():
+                    for record in record_list:
+                        await queue.put(record)
 
-                    timestamp = datetime.fromtimestamp(record.timestamp / 1000)
-
-                    try:
-                        await self.process_item(item, timestamp)
-                        counter += 1
-                    except Exception as e:
-                        span.set_status(Status(StatusCode.ERROR))
-                        span.set_attribute("error.message", str(e))
-                        span.set_attribute("error.input", record.value)
-
-                        logger.exception(f"Error processing {logging_key} {record.value}", exc_info=e)
-                        # Don't commit because we want to retry probably???
-                    else:
-                        # Doesn't matter if this fails for some reason, we can always re-process the record but we
-                        # don't want to loose any records
-                        # await run_in_background(self.consumer.kafka.commit())
-
-                        logger.debug(f"Processed for {logging_key} lag {datetime.now() - timestamp}")
-                        if counter % 1000 == 0:
-                            # TODO: Alert sentry if lag > 30 min or something? But only once... Look at avg
-                            # lag of the last few records.
-                            logger.info(f"Processed {counter} items, last lag for partition {record.partition} was {datetime.now() - timestamp}")
+                # Wait for all the tasks to complete so that we know all items have been fully processed. If one is
+                # particularly slow then this could cause some unnecessary waiting but it's a bit more robust than
+                # keeping unprocessed items in the queue from the previous kafka batch.
+                await queue.join()
