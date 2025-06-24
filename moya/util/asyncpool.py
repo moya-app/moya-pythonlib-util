fd198dac9be77c92fb2b326fd86b6e7d36f7259f
Mark Zealey <mark@markandruth.co.uk>
2025-06-24 17:48:56 +0100

    Add tests and expose the asyncpool queue where necessary

--- moya/util/asyncpool.py
+++ moya/util/asyncpool.py
@@ -3,6 +3,8 @@ import logging
 import typing as t
 from contextlib import asynccontextmanager
 
+from moya.util.background import run_in_background
+
 logger = logging.getLogger("asyncpool")
 
 T = t.TypeVar("T")
@@ -22,7 +24,7 @@ async def _run_worker(queue: asyncio.Queue[T], fn: t.Callable[[T], t.Awaitable[N
 
 
 @asynccontextmanager
-async def asyncpool(fn: t.Callable[[T], t.Awaitable[None]], worker_count: int = 5, maxsize: int = 0) -> t.AsyncIterator[t.Callable[[T], t.Awaitable[None]]]:
+async def asyncpool_queue(fn: t.Callable[[T], t.Awaitable[None]], worker_count: int = 5, maxsize: int = 0) -> t.AsyncIterator[asyncio.Queue[T]]:
     """
     Run a pool of workers to process items from a queue. The queue is returned
     from the context manager and can be used to enqueue items for processing.
@@ -40,19 +42,40 @@ async def asyncpool(fn: t.Callable[[T], t.Awaitable[None]], worker_count: int = 
     async def worker(item):
         ... do stuff ...
 
-    async def ...:
-        async with asyncpool(worker, worker_count=10) as enqueue:
+    async with asyncpool_queue(worker, worker_count=10) as queue:
+        for i in range(100):
             for i in range(100):
-                await enqueue(i)
+                await queue.put(i)
+            # ensure queue is flushed before going to next batch
+            await queue.join()
     """
 
     queue: asyncio.Queue[T] = asyncio.Queue(maxsize=maxsize)
-    workers = [asyncio.create_task(_run_worker(queue, fn)) for _ in range(worker_count)]
+    workers = [await run_in_background(_run_worker(queue, fn), name="asyncpool worker", force_run_in_background=True) for _ in range(worker_count)]
 
     try:
-        yield queue.put
+        yield queue
     finally:
         await queue.join()
         for worker in workers:
             worker.cancel()
         await asyncio.gather(*workers, return_exceptions=True)
+
+
+@asynccontextmanager
+async def asyncpool(fn: t.Callable[[T], t.Awaitable[None]], worker_count: int = 5, maxsize: int = 0) -> t.AsyncIterator[t.Callable[[T], t.Awaitable[None]]]:
+    """
+    As asyncpool_queue above but a function is returned that can be used to enqueue items for processing.
+
+    Usage:
+
+    async def worker(item):
+        ... do stuff ...
+
+    async with asyncpool(worker, worker_count=10) as enqueue:
+        for i in range(100):
+            await enqueue(i)
+    """
+
+    async with asyncpool_queue(fn, worker_count=worker_count, maxsize=maxsize) as queue:
+        yield queue.put
