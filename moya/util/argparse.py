906aeb55f81b1173b801f0752edaa751ea45543d
Joel Cross <ukch@users.noreply.github.com>
2025-07-28 20:22:05 +0300

    Add PydanticArguments class to moya.util.argparse

--- moya/util/argparse.py
+++ moya/util/argparse.py
@@ -1,6 +1,11 @@
 import argparse
 import os
-from typing import Any
+import typing as t
+
+from pydantic import ValidationError
+from pydantic_settings import CliApp, CliSettingsSource
+
+from moya.util.config import MoyaSettings
 
 
 class EnvArgumentParser(argparse.ArgumentParser):
@@ -8,8 +13,22 @@ class EnvArgumentParser(argparse.ArgumentParser):
     Like standard argparse, but assigns default values from environment variables that are prefixed with APP_ per the Moya standard.
     """
 
-    def _add_action(self, action: argparse.Action) -> Any:
+    def _add_action(self, action: argparse.Action) -> t.Any:
         env_var = "APP_" + action.dest.upper()
         action.default = os.environ.get(env_var, action.default)
         action.help += f" [env: {env_var}]"  # type: ignore
         return super()._add_action(action)
+
+
+class PydanticArguments(MoyaSettings, cli_parse_args=True, cli_kebab_case=True):
+    @classmethod
+    def run(cls) -> str | int:
+        css: CliSettingsSource[argparse.ArgumentParser] = CliSettingsSource(cls)
+        try:
+            CliApp.run(cls, cli_settings_source=css)
+        except ValidationError as e:
+            msg = ""
+            for err in e.errors():
+                msg += f"\nargument {err['loc'][0]}: {err['msg']}"
+            css.root_parser.error(msg)
+        return 0
