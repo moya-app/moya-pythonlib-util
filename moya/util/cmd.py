8d88c3716183c2d918d3555e317210341ee35944
Joel Cross <ukch@users.noreply.github.com>
2025-07-28 20:22:05 +0300

    Add PydanticArguments class to moya.util.argparse

--- moya/util/cmd.py
+++ moya/util/cmd.py
@@ -11,12 +11,16 @@ from moya.util.sentry import init as setup_sentry
 Res = t.TypeVar("Res", bound=None | int)
 
 
+def setup() -> None:
+    setup_sentry()
+    setup_logging()
+
+
 def run_async(coro: t.Awaitable[Res]) -> Res:
     """
     Set up various standard logging etc and run the specified coroutine returning its result if any.
     """
-    setup_sentry()
-    setup_logging()
+    setup()
     # Once min supported version is python 3.12 just use uvloop.run(coro)
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     return asyncio.run(coro)  # type: ignore[arg-type] # looks like linting doesnt know it can return
